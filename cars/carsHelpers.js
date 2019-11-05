90931e37375f17477bd0aaad99d07524e63f5669
Megan Ennis <Megan@Megans-iMac-9.local>
2019-11-05 14:01:41 +0000

    Helper function to get car by id

--- cars/carsHelpers.js
+++ cars/carsHelpers.js
@@ -2,8 +2,14 @@ const db = require('../data/dbConfig');
 
 module.exports = {
     get,
+    getById
 }
 
 function get() {
     return db('cars');
+}
+
+function getById(id) {
+    return db('cars')
+    .where({id})
 }

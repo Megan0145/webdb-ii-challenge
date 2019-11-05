4fcc14ed17bd233a672277145d872adfbd8da0c3
Megan Ennis <Megan@Megans-iMac-9.local>
2019-11-05 14:39:30 +0000

    Middleware function to validate request body when adding car

--- cars/carsRouter.js
+++ cars/carsRouter.js
@@ -26,22 +26,38 @@ router.get("/:id", (req, res) => {
       }
     })
     .catch(err => {
-      res
-        .status(500)
-        .json({
-          message: `Could not retrieve car with id of ${req.params.id}: ${err.message}`
-        });
+      res.status(500).json({
+        message: `Could not retrieve car with id of ${req.params.id}: ${err.message}`
+      });
     });
 });
 
-router.post("/", (req, res) => {
-    cars.insert(req.body)
+router.post("/", validateCar, (req, res) => {
+  cars
+    .insert(req.body)
     .then(car => {
-        res.status(201).json({message: "Created successully", data: car})
+      res.status(201).json({ message: "Created successully", data: car });
     })
     .catch(err => {
-        res.json(err.message)
-    })
-})
+      res.status(500).json({ message: `Could not add car: ${err.message}` });
+    });
+});
+
+//custom middleware
+function validateCar(req, res, next) {
+  if (!Object.keys(req.body).length) {
+    res.status(400).json({ message: "Missing car data" });
+  } else if (!req.body.vin) {
+    res.status(400).json({ message: "Missing required vin field" });
+  } else if (!req.body.model) {
+    res.status(400).json({ message: "Missing required model field" });
+  } else if (!req.body.mileage) {
+    res.status(400).json({ message: "Missing required mileage field" });
+  } else if (!req.body.make) {
+    res.status(400).json({ message: "Missing required make field" });
+  } else {
+    next();
+  }
+}
 
 module.exports = router;

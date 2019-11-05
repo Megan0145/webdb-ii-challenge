808ed5c0cf5f84808a2b75059c14c25f3e5ee638
Megan Ennis <Megan@Megans-iMac-9.local>
2019-11-05 13:39:37 +0000

    Add car table schema using migrations

--- knexfile.js
+++ knexfile.js
@@ -5,7 +5,7 @@ module.exports = {
   development: {
     client: 'sqlite3',
     connection: {
-      filename: './data/cars.db3'
+      filename: './data/car-dealer.db3'
     },
     useNullAsDefault: true,
     migrations: {
@@ -19,7 +19,7 @@ module.exports = {
   production: {
     client: 'sqlite3',
     connection: {
-      filename: './data/cars-production.db3'
+      filename: './data/car-dealer-production.db3'
     },
     useNullAsDefault: true,
     migrations: {

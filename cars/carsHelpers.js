b2cd47dbe93ad3b0443a8d7a932c72a552703714
Megan Ennis <Megan@Megans-iMac-9.local>
2019-11-05 14:16:38 +0000

    Helper function to insert car to db

--- cars/carsHelpers.js
+++ cars/carsHelpers.js
@@ -2,7 +2,8 @@ const db = require('../data/dbConfig');
 
 module.exports = {
     get,
-    getById
+    getById,
+    insert
 }
 
 function get() {
@@ -13,4 +14,9 @@ function getById(id) {
     return db('cars')
     .where({id})
     .first()
+}
+
+function insert(car) {
+    return db('cars')
+    .insert(car)
 }
